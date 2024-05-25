11d7e6a4dd13af33865dab97d9a2d1abe3a0b55c
ashish gurung <ashish.grg.4444@gmail.com>
2024-05-25 12:29:43 +0545

    first commit

---
+++ src/app/_app.tsx
@@ -0,0 +1,23 @@
+// import { AppProps } from 'next/app';
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//     console.log("i am loading")
+
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+
+// // import { AppProps } from 'next/app';
+// // import React from 'react';
+// // import '../styles/globals.css'; // Import global CSS (if you have any)
+
+// // function MyApp({ Component, pageProps }: AppProps) {
+// //   console.log("i am loading")
+
+// //   return <Component {...pageProps} />;
+// // }
+
+// // export default MyApp;
